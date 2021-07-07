ad0a1e2062a10442851ea75262a39918577c000d
Linuxoid85 <michail383krasnov@mail.ru>
2021-07-07 11:51:39 +0300

    Добавление порта с alsa

---
+++ multimedia/makeport.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Скрипт для создания порта
+#
+
+## Использование:
+# makeport $PACKAGE
+# $PACKAGE - имя пакета
+
+if [ -z $1 ]; then
+	if [ -z $DESKTOP ]; then
+		echo -e "\e[1;31mНе введено имя программы\e[0m"
+	else
+		yad --title="Ошибка" --text="Не введено имя программы"
+	fi
+	exit 0
+fi
+
+mkdir $1
+cd $1
+touch install
+chmod +x install
+echo "#!/bin/bash
+# Build package $1 from port
+# Port created by Linuxoid85
+#
+# (C) 2021 Michail [Linuxoid85] Krasnov <linuxoid85@gmail.com>
+
+source /usr/share/ports/core-functions.sh
+cd /usr/src
+
+wget 
+tar -xf 
+cd 
+" > install
+gedit install
+cd ../

5bafcbebcffcd9f6d4d1f89dcb75100c9ab3ee03
Administrator <michail383krasnov@mail.ru>
2021-10-08 15:32:02 +0300

    Добавление метаданных, чистка репозитория от старых файлов

--- ports/base/bc/config.sh
+++ ports/base/bc/config.sh
@@ -1,6 +1,8 @@
-NAME=bc
-VERSION=4.0.1
-DESCRIPTION="BC (basic calculator) — интерактивный интерпретатор Си-подобного языка, позволяет выполнять вычисления с произвольно заданной точностью. Часто используется как калькулятор в командной строке UNIX-подобных операционных систем."
-SITE="https://github.com/gavinhoward/bc"
-MAINTAINER=Linuxoid85
-PRIORITY=system
+{
+    "name": "bc",
+    "version": "4.0.1",
+    "description": "BC (basic calculator) — интерактивный интерпретатор Си-подобного языка, позволяет выполнять вычисления с произвольно заданной точностью. Часто используется как калькулятор в командной строке UNIX-подобных операционных систем.",
+    "site": "https://github.com/gavinhoward/bc",
+    "maintainer": "Michail Krasnov <linuxoid85@gmail.com>",
+    "priority": "system"
+}

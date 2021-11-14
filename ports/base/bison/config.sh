923989dfc565b9045e00b2e29379c3b86d3a3ae6
Linuxoid85 <michail383krasnov@mail.ru>
2021-11-14 21:35:29 +0300

    Конвертация некоторых конфигов посредством скрипта 'sh-to-json.sh'

--- ports/base/bison/config.sh
+++
@@ -1,6 +0,0 @@
-NAME=bison
-VERSION=3.7.6
-DESCRIPTION="Пакет содержит GNU версию yacc (ещё один компилятор компиляторов), необходимый для сборки некоторых пакетов"
-SITE="https://ftp.gnu.org/gnu/bison"
-MAINTAINER=Linuxoid85
-PRIORITY=system
--- ports/network/connecting/dhcpcd/config.sh
+++
@@ -1,7 +0,0 @@
-NAME=dhcpcd
-VERSION="9.1.4"
-PRIORITY="user"
-DESCRIPTION="dhcpcd is an implementation of the DHCP client specified in RFC2131. A DHCP client is useful for connecting your computer to a network which uses DHCP to assign network addresses. dhcpcd strives to be a fully featured, yet very lightweight DHCP client."
-MAINTAINER="Linuxoid85 <linuxoid85@gmail.com>"
-OPT_DEPS="LVVM-10.0.1 (with Clang) ntp-4.2.8p15 chronyd ypbind"
-FILES="$(which dhcpcd) /lib/dhcpcd/dev/udev.so /{,var/}lib/dhcpcd /usr/share/dhcpcd"

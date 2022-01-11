e87eb06ea207297e9d21fcc9b9e53b5ad7d7ea38
Linuxoid85 <michail383krasnov@mail.ru>
2022-01-11 06:02:51 +0300

    Переход на новую структуру портов

---
+++ ports/x11-wm/windowmaker/port-configuration.sh
@@ -0,0 +1,6 @@
+# Begin /usr/ports/x11-wm/windowmaker/port-configuration.sh
+
+PREFIX="/usr" # The installation prefix
+LANGS="en ru" # The supported languages (see './configure LINGUAS="list"' for getting more info)
+
+# End /usr/ports/x11-wm/windowmaker/port-configuration.sh
---
+++ ports/x11/XBS/port-configuration.sh
@@ -0,0 +1,7 @@
+# Begin /usr/ports/x11/XBS/port-configuration.sh
+
+PREFIX="/opt/X11"    # Install destination
+DEFAULT_PREFIX="yes" # '$PREFIX' (line #3) is standart prefix
+BUILD_KDE="no"       # Don't building a KDE Desktop
+
+# End /usr/ports/x11/XBS/port-configuration.sh
